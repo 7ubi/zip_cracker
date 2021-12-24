840a4bcfe7a73fec4cf2d294e2803ae3e4272ae3
7ubi <tobi.renner03@gmail.com>
2021-12-24 23:37:33 +0100

    output fixes

--- zip_cracker.py
+++ zip_cracker.py
@@ -1,8 +1,9 @@
 import sys
+import time
 import zipfile
 from tqdm import tqdm
 
-passwordlist = ''
+# Get zip file
 zip_file = None
 if len(sys.argv) > 1:
     zip_file = sys.argv[1]
@@ -10,10 +11,13 @@ else:
     print("ERROR: PLEASE ENTER A ZIP FILE")
     exit(0)
 
+# Get password list
 # Only sets password list if argument is given in command
+passwordlist = ''
 if len(sys.argv) > 2:
     passwordlist = sys.argv[2]
 
+# Check if zip file exists
 try:
     zip_file = zipfile.ZipFile(zip_file)
 except FileNotFoundError:
@@ -24,6 +28,11 @@ def bruteForce(possible, password_max_length, title):
     print(title)
 
     password_possibilities = pow(len(possible), password_max_length)
+    start_time = time.time()
+
+    for i in range(password_max_length - 1):
+        password_possibilities += pow(len(possible), (i + 1))
+
     password_tested = 0
 
     for i in range(password_max_length):
@@ -31,12 +40,12 @@ def bruteForce(possible, password_max_length, title):
 
         while indices[0] < len(possible):
             # create password
-            password = ""
+            pwd = ""
             for j in range(len(indices)):
-                password += possible[indices[j]]
+                pwd += possible[indices[j]]
 
             indices[len(indices) - 1] += 1
-            #print(password)
+
             if len(indices) > 1:
                 if indices[len(indices) - 1] == len(possible):
                     for j in range(len(indices) - 1, 0, -1):
@@ -48,26 +57,31 @@ def bruteForce(possible, password_max_length, title):
                             break
 
             password_tested += 1
-            # percentage = round(password_tested / password_possibilities * 10000) / 100
-            sys.stdout.write(f"\rpasswords tested: {password_tested}") # / {password_possibilities} - {percentage}%")
+            percentage = round(password_tested / password_possibilities * 10000) / 100
+            time_spent = round((time.time() - start_time) * 100) / 100
+            sys.stdout.write(f"\rpasswords tested: {password_tested} / {password_possibilities} - {percentage}% -"
+                             f" [{time_spent} s]")
             sys.stdout.flush()
 
             try:
-                zip_file.extractall(pwd=str.encode(password))
+                zip_file.extractall(pwd=str.encode(pwd))
             except (RuntimeError, zipfile.BadZipFile):
                 continue
             else:
-                print(f"\npassword: {password}")
+                print(f"\npassword: {pwd}")
                 exit(0)
 
 
 # Crack password of zip with password list file
 if passwordlist != '':
+    num_lines = 0
     try:
         num_lines = len(list(open(passwordlist, "rb")))
     except FileNotFoundError:
         print("ERROR: PASSWORD LIST FILE NOT FOUND")
         exit(0)
+
+    final_password = ""
 
     # loops through every password in given file
     with open(passwordlist, "rb") as passwords:
@@ -78,15 +92,17 @@ if passwordlist != '':
             except (RuntimeError, zipfile.BadZipFile):
                 continue
             else:
-                print(f"\npassword: {password.decode().strip()}")
-                exit(0)
+                final_password = password
+                break
+    print(f"password: {final_password.decode().strip()}")
+    exit(0)
 else:
     # Possible symbols in password
     lowercase_letters = 'abcdefghijklmnopqrstuvwxyz'
     uppercase_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     numbers = '0123456789'
     # other = '![]{}()%&*$#^<>~@|'
-    password_max_length = 4
+    password_max_length = 7
 
     bruteForce(numbers, password_max_length, "Checking for passwords with only numbers")
     bruteForce(lowercase_letters, password_max_length, "\nChecking for passwords with only lowercase letters")
